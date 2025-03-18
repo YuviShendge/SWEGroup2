2b87859d21d9b94922df1a2a666fa7453f579e35
Yuvi <you@exampyuvi.shen.dge@gmail.com>
2025-03-18 17:23:34 -0400

    allows 1 emal per user and created home page routing

---
+++ frontend/src/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <h1>Welcome to Home Page</h1>
+    </div>
+  );
+}
+
+export default Home;
